63d8ca187e28c17fa5a2c6e731d1d1bedac5dda2
Engineer-Nick <fastwriter001.com>
2023-08-17 17:36:03 -0700

    Fizz

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,18 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+  * print_line - straight -line
+  * @n: Number of times
+  * Return: Always 0
+  */
+
+void print_line(int n)
 {
-	print_line(0);
-	print_line(2);
-	print_line(10);
-	print_line(-4);
-	return (0);
+	int i;
+
+		for (i = 0; i < n; x++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
